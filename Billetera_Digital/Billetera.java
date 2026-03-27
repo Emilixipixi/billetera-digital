838a0e38b5e1df7ed0ba8aca2b1e81de4eb95353
Emilixipixi <carlosemilio270798@gmail.com>
2026-03-26 19:57:42 -0500

    feat: Add constructor to Billetera class for initialization

--- Billetera_Digital/Billetera.java
+++ Billetera_Digital/Billetera.java
@@ -4,5 +4,13 @@ public class Billetera {
     String propietario;   
     String numeroCuenta;   
     double saldo;          
-    boolean activa;        
+    boolean activa;
+    
+    
+    public Billetera(String propietario, String numeroCuenta, double saldoInicial){
+        this.propietario = propietario;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldoInicial;
+        this.activa = true;
+    }
 }
