0ef6fbd1537fec4d36b719e8a4687988c43431da
Misael97001 <chiluisajefferson97@gmail.com>
2026-03-26 19:50:03 -0500

    feat: atributos getters and setters

--- contacto.java
+++ contacto.java
@@ -12,4 +12,37 @@ public class Contacto {
 		this.banco = banco;
 		this.favorito = false;
 	}
+
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuentaDestino() {
+		return cuentaDestino;
+	}
+
+	public void setCuentaDestino(String cuentaDestino) {
+		this.cuentaDestino = cuentaDestino;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public boolean isFavorito() {
+		return favorito;
+	}
+
+	public void setFavorito(boolean favorito) {
+		this.favorito = favorito;
+	}
+
 }
